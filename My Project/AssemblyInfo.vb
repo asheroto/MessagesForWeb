e74a5bd1f06578085c4e29401e788232673b3531
asheroto <github@asheroto.com>
2022-04-19 22:19:32 -0500

    Version fix

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.0.6.2")>
-<Assembly: AssemblyFileVersion("0.0.6.2")>
+<Assembly: AssemblyVersion("0.0.6.5")>
+<Assembly: AssemblyFileVersion("0.0.6.5")>

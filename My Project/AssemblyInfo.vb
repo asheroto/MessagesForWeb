aa373b6c2715d735bff6ad96cf31a73f1791b41d
asheroto <github@asheroto.com>
2022-09-01 14:01:59 -0500

    Optimized restart functionality

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("0.0.7.2")>
-<Assembly: AssemblyFileVersion("0.0.7.2")>
+<Assembly: AssemblyVersion("0.0.7.3")>
+<Assembly: AssemblyFileVersion("0.0.7.3")>
